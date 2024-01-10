442f46694984bd2e03367b354951a4381322b09f
Alessandro Fernandes <152189261+Alessandroff4@users.noreply.github.com>
2024-01-10 09:12:03 -0300

    Add files via upload

---
+++ Aula 1/Tabuada de loop/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada_de_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            int contador = 0;
+            Console.WriteLine("Digite um número de 0 a 10 que você deseja saber a tabuada. ");
+            // comando para a entrada do numero
+            numero = int.Parse(Console.ReadLine());
+            // comando para verificar se o numero digitado e de 1 a 10
+            if (numero < 0 || numero > 10)
+            {
+                Console.WriteLine("Por fazor digite um número de 0 a 10");
+            }
+            // o comando so vai executar se o if for falso
+            else
+            {
+
+                // usei o comando while pro programa rodar codigo entre as chaves
+                // ate que a condição seja falsa
+                while (contador < 11)
+                {
+                    Console.WriteLine(numero + " X " + contador + " = " + numero * contador);
+
+                    contador = contador + 1;
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Aula 2/Atividade 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Markup;
+using System.Threading.Tasks;
+
+namespace Atividade_1
+{
+    internal class Program
+    
+    { // Ativadide pra o uso de variaveis, cauculo de media de idade
+        static void Main(string[] args)
+        {
+        int idadeJoao = 10;
+        int idadeMaria = 25;
+        int idadeCarlos = 30;
+        // Calcule a média das idades.
+        double mediaIdades = (idadeJoao + idadeMaria + idadeCarlos) / 3.0;
+        // Exiba o resultado em um MessageBox.
+        Console.WriteLine("A média das idades é: " + mediaIdades);
+        Console.ReadLine();
+    }
+    }
+}
