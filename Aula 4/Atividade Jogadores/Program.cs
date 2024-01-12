905674f42a05c9b02f63b234b60b997e75add172
Alessandro Fernandes <152189261+Alessandroff4@users.noreply.github.com>
2024-01-12 09:20:45 -0300

    Add files via upload

---
+++ Aula 4/Atividade Jogadores/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Jogadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe sua idade: ");
+
+            // Lê e converte para int
+            if (int.TryParse(Console.ReadLine(), out int idade))
+            {
+                // Verifica a categoria
+                if (idade <= 18)
+                {
+                    Console.WriteLine("Você jogará na categoria Juvenil.");
+                }
+                else if (idade >= 23)
+                {
+                    Console.WriteLine("Você pode jogar na categoria Profissional.");
+                }
+                else
+                {
+                    Console.WriteLine("Você não está na categoria Juvenil, mas também não está na Profissional.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Por favor, insira uma idade válida.");
+            }
+                Console.ReadKey();
+        }
+    }
+}
